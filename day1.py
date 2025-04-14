55853ddec86d8c9bf53657fbbeebd73079c17b4b
Bishaleyyyyy <techbishal566@gmail.com>
2025-04-14 18:02:49 +0545

    do some practice

--- day1.py
+++ day1.py
@@ -1,7 +1,23 @@
+#variables and data types
+
 hehe = "haha"
 lol = "lulli",
 isNepali = True
 firstName = "Bishal"
 lastName = "Kc"
 
+#printing
 print(f"my name is {firstName} {lastName}")
+
+
+friend1 = "pranjal"
+friend1 = "ashish"
+friend1 = "nabin"
+friend1 = "bikash"
+
+#if same type of data insert , use list
+#list
+friends = ["pranjal","ashish","nabin","bikash"]
+
+print(friends)
+print(friends[1])
