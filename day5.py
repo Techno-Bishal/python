e95f1b212d42335d5f6f04130d0d870161838670
Bishaleyyyyy <techbishal566@gmail.com>
2025-04-17 16:46:35 +0545

    Day 5 completed

---
+++ day5.py
@@ -0,0 +1,34 @@
+# def add(a,b):
+#      return a+b 
+
+# result = add(1,2)
+# print(result)
+
+# class Calculation :
+#      def add(self,a,b):
+#           return a+b
+     
+# calculator = Calculation()
+# result2 = calculator.add(2,3)
+# print(result2)
+
+
+#Inheritance
+class Animal:
+    def __init__(self,name):
+        print("Animal constructor is called ", name)
+        self.name = name
+
+    def eat(self):
+            print("I can eat")
+
+    def sleep(self):
+                print("I can sleep")
+
+class Dog(Animal):
+    def bark(self):
+        print(" I can bark..")
+
+dog1 = Dog("German Shephered.")
+dog1.eat()
+    
